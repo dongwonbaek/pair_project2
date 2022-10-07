a1fb911bd3ef48e416af4a4755f298995997b69d
hvvany <kjunhwan1998@gmail.com>
2022-10-07 10:11:26 +0900

    base작성

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class Movie(models.Model):
+  title = models.CharField(max_length=20)
+  content = models.TextField()
+  movie_name = models.CharField(max_length=20)
+  grade = models.IntegerField(default=10,validators=[MinValueValidator(0),MaxValueValidator(10)])
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
--- pjt/settings.py
+++ pjt/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_bootstrap5',
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'pjt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
