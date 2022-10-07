322d9411373b91208149b4c16e188090ef91d878
hvvany <kjunhwan1998@gmail.com>
2022-10-07 15:46:53 +0900

    finish review_function

--- movies/forms.py
+++ movies/forms.py
@@ -6,4 +6,10 @@ class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = '__all__'
-        exclude = ['created_at', 'updated_at']
+        exclude = ['created_at', 'updated_at', 'view_count']
+        labels = {
+        'title' : '리뷰 제목',
+        'content' : '리뷰 내용',
+        'movie_name' : '영화 제목',
+        'grade' : '평점(0~10)',
+        }
