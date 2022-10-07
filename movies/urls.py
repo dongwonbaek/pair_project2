0665e14414d205c81870a45b35de508c6abc1dee
an dong woo <akskwha@naver.com>
2022-10-07 11:11:50 +0900

    네비바 구현

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,8 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('index/', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    # path('<int:pk>/', views.cerate, name=''),
+    # path('<int:pk>/update/ ', views.cerate, name='update'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def main(request):
 
   return render(request, 'movies/main.html')
+
+def index(request):
+
+  return render(request, 'movies/index.html')
+
+def create(request):
+
+  return render(request, 'movies/create.html')
